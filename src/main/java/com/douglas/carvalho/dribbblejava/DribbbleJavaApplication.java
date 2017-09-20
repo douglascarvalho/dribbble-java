e1f92c67412f5e8390446378d4f050cc8783eb26
Douglas Carvalho <douglaspdc@gmail.com>
2017-09-20 12:21:13 -0300

    add project structure

---
+++ src/main/java/com/douglas/carvalho/dribbblejava/DribbbleJavaApplication.java
@@ -0,0 +1,12 @@
+package com.douglas.carvalho.dribbblejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DribbbleJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DribbbleJavaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/douglas/carvalho/dribbblejava/DribbbleJavaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.douglas.carvalho.dribbblejava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DribbbleJavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
