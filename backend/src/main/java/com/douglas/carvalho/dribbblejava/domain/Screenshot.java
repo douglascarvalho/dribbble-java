16e6eedc2352f42a50767d885940f3f17ae67a48
Douglas Carvalho <douglaspdc@gmail.com>
2017-09-22 15:47:54 -0300

    add author domain object to get user

--- backend/src/main/java/com/douglas/carvalho/dribbblejava/domain/Screenshot.java
+++ backend/src/main/java/com/douglas/carvalho/dribbblejava/domain/Screenshot.java
@@ -1,10 +1,27 @@
 package com.douglas.carvalho.dribbblejava.domain;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Screenshot {
 	
 	private String title;
 
+	@SerializedName("user")
+	private Author author;
+	
+	@SerializedName("created_at")
+	private String publishDate;
+	
 	private Image images;
+	
+	@SerializedName("views_count")
+	private Integer viewsCount;
+	
+	@SerializedName("likes_count")
+	private Integer likesCount;
+	
+	@SerializedName("buckets_count")
+	private Integer bucketsCount;
 	
 	public String getTitle() {
 		return title;
@@ -20,6 +37,46 @@ public class Screenshot {
 
 	public void setImages(Image images) {
 		this.images = images;
+	}
+
+	public String getPublishDate() {
+		return publishDate;
+	}
+
+	public void setPublishDate(String publishDate) {
+		this.publishDate = publishDate;
+	}
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+
+	public Integer getViewsCount() {
+		return viewsCount;
+	}
+
+	public void setViewsCount(Integer viewsCount) {
+		this.viewsCount = viewsCount;
+	}
+
+	public Integer getLikesCount() {
+		return likesCount;
+	}
+
+	public void setLikesCount(Integer likesCount) {
+		this.likesCount = likesCount;
+	}
+
+	public Integer getBucketsCount() {
+		return bucketsCount;
+	}
+
+	public void setBucketsCount(Integer bucketsCount) {
+		this.bucketsCount = bucketsCount;
 	}
 	
 }
